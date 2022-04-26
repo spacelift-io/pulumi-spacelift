80aa07746a248b146ecd29431afb6c7fc348b5b0
Jakub Martin <kubam@spacelift.io>
2022-04-26 11:44:56 +0200

    Release new version.

--- sdk/python/bin/setup.py
+++ sdk/python/bin/setup.py
@@ -32,7 +32,7 @@ def readme():
 
 
 setup(name='pulumi_spacelift',
-      version='1.2.4',
+      version='1.2.5',
       description="A Pulumi package for creating and managing Spacelift resources.",
       long_description=readme(),
       long_description_content_type='text/markdown',
