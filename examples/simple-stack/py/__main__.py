d842dc5b47591e49221b209be7982877ae158d3f
James Humphries <jamesh@spacelift.io>
2022-10-11 09:25:59 +0100

    added examples

--- examples/simple-stack/py/__main__.py
+++ examples/simple-stack/py/__main__.py
@@ -1,3 +1,15 @@
 """A Python Pulumi program"""
 
 import pulumi
+from pulumi_spacelift import Stack
+
+stack = Stack("my-stack",
+  administrative=False,
+  autodeploy=False,
+  branch="main",
+  description="A simple stack",
+  name="simple-stack-python-pulumi",
+  repository="empty",
+  project_root="",
+  terraform_version="1.3.0"
+)
